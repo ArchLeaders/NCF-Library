4e73f0ff1319574b05f82ce4a30dcbc763effca4
Marcus Smith <80713508+ArchLeaders@users.noreply.github.com>
2022-06-01 23:07:31 -0700

    Remove oead interop files

--- BymlLibrary/Interop/.build/lib/oead/lib/libyaml/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Visual Studio/Libraries/EADLibrary/BymlLibrary/Interop/lib/oead/lib/libyaml
-# Build directory: D:/Visual Studio/Libraries/EADLibrary/BymlLibrary/Interop/.build/lib/oead/lib/libyaml
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
