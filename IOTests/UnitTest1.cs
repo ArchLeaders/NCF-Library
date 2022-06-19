60bad15f5aa97d8fae32f32008ecf7084d0ddd76
GingerAvalanche <chodness@gmail.com>
2022-06-19 01:08:23 -0700

    Migrate test project to Visual Studio Test Explorer

---
+++ IOTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nintendo.Aamp;
+using Nintendo.Bfres;
+using Nintendo.Byml;
+using Nintendo.Sarc;
+using Nintendo.Yaz0;
+using System.Diagnostics;
+using System.IO;
+
+namespace IOTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Aamp()
+        {
+            AampFile aamp = new(@"..\..\..\Data\IO.aamp");
+            Trace.WriteLine(aamp.ToYml());
+        }
+        [TestMethod]
+        public void Byml()
+        {
+            BymlFile byml = new(@"..\..\..\Data\IO.byml");
+            Trace.WriteLine(byml.ToYaml());
+        }
+        [TestMethod]
+        public void NewAamp()
+        {
+            AampFile aamp = AampFile.New(2);
+            File.WriteAllBytes(@"..\..\..\Data\new.aamp", aamp.ToBinary());
+        }
+        [TestMethod]
+        public void NewByml()
+        {
+            BymlFile byml = new(NodeType.Dictionary);
+            File.WriteAllBytes(@"..\..\..\Data\new.byml", byml.ToBinary());
+        }
+    }
+}
