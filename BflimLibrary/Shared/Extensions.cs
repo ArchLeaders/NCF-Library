5d24ceadbdaff1f3630eb95ec5a8c0bf959353d7
ArchLeaders <archparliament@gmail.com>
2022-08-30 21:32:47 -0700

    Stupid `Floor` shorthand

--- BflimLibrary/Shared/Extensions.cs
+++ BflimLibrary/Shared/Extensions.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BntxLibrary.Shared
+﻿namespace BntxLibrary.Shared
 {
-    public static class Extensions
+    internal static class Extensions
     {
         public static int ToInt(this decimal _decimal) => Convert.ToInt32(_decimal);
         public static int ToInt(this uint _uint) => Convert.ToInt32(_uint);
-
         public static uint ToUInt(this decimal _decimal) => Convert.ToUInt32(_decimal);
+
+        public static int FloorFunction(dynamic value) => decimal.Floor(value).ToInt();
+        public delegate int FloorAction(dynamic value);
+        public static FloorAction Floor = FloorFunction;
     }
 }
